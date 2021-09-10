56733e986e4336618cea70cfb359bb5c04e5d557
Matteo Gassend <matteo.gassend@epitech.eu>
2021-09-10 15:47:52 +0200

    [add] navigation to movie details onTap

--- lib/movie_discovery.dart
+++ lib/movie_discovery.dart
@@ -14,22 +14,27 @@ class MovieCard extends StatelessWidget {
     final double blur = active ? 30 : 0;
     final double offset = active ? 20 : 0;
     final double top = active ? 100 : 200;
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.easeOutQuint,
-      margin: EdgeInsets.only(top: top, bottom: 50, right: 30),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage(m.getPoster()),
-          ),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black87,
-                blurRadius: blur,
-                offset: Offset(offset, offset))
-          ]),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, "/movie/details", arguments: {"id": m.id});
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeOutQuint,
+        margin: EdgeInsets.only(top: top, bottom: 50, right: 30),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(m.getPoster()),
+            ),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black87,
+                  blurRadius: blur,
+                  offset: Offset(offset, offset))
+            ]),
+      ),
     );
   }
 }
