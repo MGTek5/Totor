8328494851c9cb306a648076e0f0ed49006f3d2f
Quentin <quentin.henry@epitech.eu>
2021-09-21 16:14:40 +0200

    Add new component input

--- lib/components/input.dart
+++ lib/components/input.dart
@@ -10,18 +10,30 @@ class Input extends StatelessWidget {
     return Container(
       margin: const EdgeInsetsDirectional.all(12),
       alignment: Alignment.center,
+<<<<<<< master
       child: TextField(
+=======
+      child: const TextField(
+>>>>>>> Add new component input
         obscureText: true,
         decoration: InputDecoration(
           filled: true,
           fillColor: Colors.white,
+<<<<<<< master
           border: const OutlineInputBorder(
+=======
+          border: OutlineInputBorder(
+>>>>>>> Add new component input
             borderRadius: BorderRadius.all(
               Radius.circular(18.0),
             ),
             borderSide: BorderSide.none,
           ),
+<<<<<<< master
           hintText: text,
+=======
+          hintText: 'Password',
+>>>>>>> Add new component input
           floatingLabelBehavior: FloatingLabelBehavior.never,
         ),
       ),
