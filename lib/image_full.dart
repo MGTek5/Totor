9da40d080142e736daddbc215a9157de73dc348f
Matteo <matteo.gassend@epitech.eu>
2021-09-24 19:36:03 +0200

    [add] fullscreen image screen

---
+++ lib/image_full.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:totor/arguments.dart';
+
+class ImageFull extends StatelessWidget {
+  const ImageFull({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ImageFullArguments args =
+        ModalRoute.of(context)!.settings.arguments as ImageFullArguments;
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Image.network(args.path),
+        ),
+      ),
+    );
+  }
+}
