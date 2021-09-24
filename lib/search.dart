8032afb1a4247c0216e7c6442825a8622826e6d4
Matteo <matteo.gassend@epitech.eu>
2021-09-24 11:34:15 +0200

    [add] layout to mimic movie discovery page

--- lib/search.dart
+++ lib/search.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 import 'api.dart';
 import 'nav_list.dart';
+
+class MovieCard extends StatelessWidget {
+  const MovieCard({Key? key, required this.m, required this.active})
+      : super(key: key);
+
+  final Movie m;
+  final bool active;
+
+  @override
+  Widget build(BuildContext context) {
+    final double blur = active ? 15 : 0;
+    final double offset = active ? 7 : 0;
+    final double top = active ? 100 : 200;
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, "/movie/details", arguments: {"id": m.id});
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeOutQuint,
+        margin: EdgeInsets.only(top: top, bottom: 50, right: 30),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(m.getPoster()),
+            ),
+            boxShadow: [
+              BoxShadow(
+                  color: const Color(0xffEEB868).withOpacity(0.3),
+                  blurRadius: blur,
+                  offset: Offset(offset, offset))
+            ]),
+      ),
+    );
+  }
+}
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -24,20 +62,12 @@ class _SearchState extends State<Search> {
         loading = true;
       });
       instance.searchMovie(query: searchController.text).then((value) {
-        List<ListTile> tmp = [];
-
-        for (var element in value) {
-          tmp.add(ListTile(
-              title: Text(element.title),
-              leading: Image.network(element.getPoster()),
-              onTap: () {
-                Navigator.pushNamed(context, "/movie/details",
-                    arguments: {"id": element.id});
-              }));
+        if (searchResults.isNotEmpty) {
+          pageController.animateToPage(0,
+              curve: Curves.ease, duration: Duration(seconds: 1));
         }
-
         setState(() {
-          searchResults = tmp;
+          searchResults = value;
           loading = false;
         });
       }).catchError((error) {
@@ -48,20 +78,35 @@ class _SearchState extends State<Search> {
     }
   }
 
-  List<ListTile> searchResults = [];
+  List<Movie> searchResults = [];
   bool loading = false;
   TextEditingController searchController = TextEditingController();
+  PageController pageController = PageController(viewportFraction: 0.95);
+  int currentPage = 0;
+  int lastPage = 1;
 
   @override
   void initState() {
     super.initState();
     searchController.addListener(handleSearchChange);
+    pageController.addListener(() {
+      int next = pageController.page!.round();
+      if (currentPage != next) {
+        setState(() {
+          currentPage = next;
+        });
+      }
+    });
   }
 
   @override
   void dispose() {
     super.dispose();
     searchController.dispose();
+  }
+
+  Widget _buildMoviePage(Movie m, bool active) {
+    return MovieCard(m: m, active: active);
   }
 
   Widget buildSearchInput() {
@@ -85,19 +130,25 @@ class _SearchState extends State<Search> {
         currentIndex: getRouteIndex(context: context),
       ),
       body: SafeArea(
-          child: SingleChildScrollView(
-        primary: true,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            buildSearchInput(),
-            if (loading) ...[const CircularProgressIndicator()],
-            if (searchResults.isEmpty)
-              const Text(
-                  "Nothing to see here, start typing to see some results"),
-            ...searchResults
-          ],
-        ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          buildSearchInput(),
+          if (loading) ...[const CircularProgressIndicator()],
+          if (searchResults.isEmpty)
+            const Text("Nothing to see here, start typing to see some results"),
+          if (searchResults.isNotEmpty)
+            Expanded(
+              child: PageView.builder(
+                controller: pageController,
+                itemCount: searchResults.length,
+                itemBuilder: (context, int currentIdx) {
+                  bool active = currentIdx == currentPage;
+                  return _buildMoviePage(searchResults[currentIdx], active);
+                },
+              ),
+            )
+        ],
       )),
     ));
   }
