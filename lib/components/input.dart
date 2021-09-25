76e685cd26d6c98b7911f549e78c47720e57d0a0
Dythic <quentin.henry@epitech.eu>
2021-09-25 19:19:06 +0200

    [add] text varible in hint text

--- lib/components/input.dart
+++ lib/components/input.dart
@@ -10,42 +10,18 @@ class Input extends StatelessWidget {
     return Container(
       margin: const EdgeInsetsDirectional.all(12),
       alignment: Alignment.center,
-<<<<<<< master
-<<<<<<< master
       child: TextField(
-=======
-      child: const TextField(
->>>>>>> Add new component input
-=======
-      child: TextField(
->>>>>>> [add] text varible in hint text
         obscureText: true,
         decoration: InputDecoration(
           filled: true,
           fillColor: Colors.white,
-<<<<<<< master
-<<<<<<< master
           border: const OutlineInputBorder(
-=======
-          border: OutlineInputBorder(
->>>>>>> Add new component input
-=======
-          border: const OutlineInputBorder(
->>>>>>> [add] text varible in hint text
             borderRadius: BorderRadius.all(
               Radius.circular(18.0),
             ),
             borderSide: BorderSide.none,
           ),
-<<<<<<< master
-<<<<<<< master
           hintText: text,
-=======
-          hintText: 'Password',
->>>>>>> Add new component input
-=======
-          hintText: text,
->>>>>>> [add] text varible in hint text
           floatingLabelBehavior: FloatingLabelBehavior.never,
         ),
       ),
