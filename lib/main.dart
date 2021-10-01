182be49b85c135526552c4ebbc2ebb255f853f70
Matteo Gassend <matteo.gassend@epitech.eu>
2021-10-01 17:14:42 +0200

    [wip] removed shared_preferences and reinstated test

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:totor/image_full.dart';
 import 'package:totor/models/user.dart';
 import 'package:totor/movie_details.dart';
 import 'package:totor/search.dart';
-
 import 'intro_screens.dart';
 import 'movie_discovery.dart';
 
 void main() async {
-  final SharedPreferences sharedPreferences =
-      await SharedPreferences.getInstance();
-  runApp(MyApp(sPreferences: sharedPreferences));
+  await GetStorage.init();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key, required this.sPreferences}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-  final SharedPreferences sPreferences;
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<User>(
-          create: (_) => User(),
-        )
-      ],
-      child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            brightness: Brightness.dark,
-          ),
-          initialRoute: "/",
-          routes: {
-            "/": (context) => sPreferences.getBool("introSeen") ?? false
-              ? const MovieDiscovery()
-              : const IntroScreens(),
-            "/search": (context) => const Search(),
-            "/movie/details": (context) => const MovieDetails(),
-            "/imagefull": (context) => const ImageFull()
-          })
-    );
+        providers: [
+          ChangeNotifierProvider<User>(
+            create: (_) => User(),
+          )
+        ],
+        child: MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              brightness: Brightness.dark,
+            ),
+            initialRoute: "/",
+            routes: {
+              "/": (context) => GetStorage().read<bool>("introSeen") ?? false
+                  ? const MovieDiscovery()
+                  : const IntroScreens(),
+              "/search": (context) => const Search(),
+              "/movie/details": (context) => const MovieDetails(),
+              "/imagefull": (context) => const ImageFull()
+            }));
   }
 }
