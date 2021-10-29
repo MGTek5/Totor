635d80952456f0f24d43e1b40153d2c813af1ab1
Matteo <matteo.gassend@epitech.eu>
2021-10-29 15:05:07 +0200

    [add] loading indicator on login screen

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -17,85 +17,114 @@ class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final _password = TextEditingController();
   final _email = TextEditingController();
+  bool _loading = false;
+
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
       body: SafeArea(
           child: SingleChildScrollView(
         physics: const ClampingScrollPhysics(),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 200, child: Image.asset("assets/icon.png")),
-              const Text(
-                "Welcome Back!",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-              ),
-              const Text("Sign in with your email and password"),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    CustomFormField(
-                      controller: _email,
-                      hintText: "Email",
-                      validator: (String? value) {
-                        if (value!.isEmpty ||
-                            !value.contains("@") ||
-                            !value.contains(".")) {
-                          return 'Invalid email';
-                        }
-                        return null;
-                      },
-                    ),
-                    CustomFormField(
-                      controller: _password,
-                      hintText: "Password",
-                      obscureText: true,
-                      validator: (String? value) {},
-                    ),
-                    Button("Login", () async {
-                      _formKey.currentState!.save();
-                      if (_formKey.currentState!.validate()) {
-                        try {
-                          dynamic res = await instance.login(
-                            _email.text,
-                            _password.text,
-                          );
-                          User user = Provider.of<User>(context, listen: false);
-                          user.setEmail(res["email"]);
-                          user.setId(res["id"]);
-                          user.setProfilePic(res["profilePic"]);
-                          user.setUsername(res['username']);
-                          user.setLogged(true);
-                          GetStorage().write("loggedIn", true);
-                          GetStorage().write("user", user.toJson());
-                          Navigator.pushNamed(context, '/');
-                        } catch (e) {
-                          showDialog(
-                              context: context,
-                              builder: (ctx) {
-                                return AlertDialog(
-                                  title: const Text("Could not sign in"),
-                                  content: Text(e.toString()),
-                                );
+        child: Stack(
+          children: [
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 200, child: Image.asset("assets/icon.png")),
+                  const Text(
+                    "Welcome Back!",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  const Text("Sign in with your email and password"),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        CustomFormField(
+                          controller: _email,
+                          hintText: "Email",
+                          validator: (String? value) {
+                            if (value!.isEmpty ||
+                                !value.contains("@") ||
+                                !value.contains(".")) {
+                              return 'Invalid email';
+                            }
+                            return null;
+                          },
+                        ),
+                        CustomFormField(
+                          controller: _password,
+                          hintText: "Password",
+                          obscureText: true,
+                          validator: (String? value) {},
+                        ),
+                        Button("Login", () async {
+                          _formKey.currentState!.save();
+                          if (_formKey.currentState!.validate()) {
+                            try {
+                              setState(() {
+                                _loading = true;
                               });
-                        }
-                      }
-                    }),
+                              dynamic res = await instance.login(
+                                _email.text,
+                                _password.text,
+                              );
+                              User user =
+                                  Provider.of<User>(context, listen: false);
+                              user.setEmail(res["email"]);
+                              user.setId(res["id"]);
+                              user.setProfilePic(res["profilePic"]);
+                              user.setUsername(res['username']);
+                              user.setLogged(true);
+                              GetStorage().write("loggedIn", true);
+                              GetStorage().write("user", user.toJson());
+                              setState(() {
+                                _loading = false;
+                              });
+                              Navigator.pushNamed(context, '/');
+                            } catch (e) {
+                              showDialog(
+                                  context: context,
+                                  builder: (ctx) {
+                                    return AlertDialog(
+                                      title: const Text("Could not sign in"),
+                                      content: Text(e.toString()),
+                                    );
+                                  });
+                            }
+                          }
+                        }),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          primary: Colors.orange,
+                          textStyle: const TextStyle(fontSize: 12)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/register");
+                      },
+                      child: const Text("Create an account"))
+                ]),
+            if (_loading)
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(color: Colors.black.withOpacity(0.9)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    ),
+                    Text("Loading, please wait")
                   ],
                 ),
-              ),
-              TextButton(
-                  style: TextButton.styleFrom(
-                      primary: Colors.orange,
-                      textStyle: const TextStyle(fontSize: 12)),
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/register");
-                  },
-                  child: const Text("Create an account"))
-            ]),
+              )
+          ],
+        ),
       )),
     ));
   }
