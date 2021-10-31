4bf11bf6051b5f09c25215e51de523ae000d849f
Matteo <matteo.gassend@epitech.eu>
2021-10-31 11:04:52 +0100

    [refact] more customization for MovieCard

--- lib/components/movie_card.dart
+++ lib/components/movie_card.dart
@@ -4,21 +4,44 @@ import 'package:totor/utils/arguments.dart';
 import 'package:totor/models/movie.dart';
 
 class MovieCard extends StatelessWidget {
-  const MovieCard({Key? key, required this.m, required this.active})
-      : super(key: key);
+  const MovieCard(
+      {Key? key,
+      required Movie movie,
+      required bool active,
+      double activeTop = 100,
+      double inactiveTop = 200,
+      double activeBlur = 15,
+      double inactiveBlur = 0,
+      double activeOffset = 7,
+      double inactiveOffset = 0})
+      : _movie = movie,
+        _active = active,
+        _activeTop = activeTop,
+        _inactiveTop = inactiveTop,
+        _activeBlur = activeBlur,
+        _inactiveBlur = inactiveBlur,
+        _activeOffset = activeOffset,
+        _inactiveOffset = inactiveOffset,
+        super(key: key);
 
-  final Movie m;
-  final bool active;
+  final Movie _movie;
+  final bool _active;
+  final double _activeTop;
+  final double _inactiveTop;
+  final double _activeOffset;
+  final double _inactiveOffset;
+  final double _activeBlur;
+  final double _inactiveBlur;
 
   @override
   Widget build(BuildContext context) {
-    final double blur = active ? 15 : 0;
-    final double offset = active ? 7 : 0;
-    final double top = active ? 100 : 200;
+    final double blur = _active ? _activeBlur : _inactiveBlur;
+    final double offset = _active ? _activeOffset : _inactiveOffset;
+    final double top = _active ? _activeTop : _inactiveTop;
     return GestureDetector(
       onTap: () {
         Navigator.pushNamed(context, "/movie/details",
-            arguments: MovieDetailsArguments(m.id));
+            arguments: MovieDetailsArguments(_movie.id));
       },
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 500),
@@ -28,7 +51,7 @@ class MovieCard extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
             image: DecorationImage(
               fit: BoxFit.cover,
-              image: NetworkImage(m.getPoster()),
+              image: NetworkImage(_movie.getPoster()),
             ),
             boxShadow: [
               BoxShadow(
