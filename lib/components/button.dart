d0dce35624d364ccbf2b6e19399eb119faaa1492
Matteo <matteo.gassend@epitech.eu>
2021-10-31 19:06:18 +0100

    [refact] using custom color for button

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:totor/utils/custom_colors.dart';
 
 class Button extends StatelessWidget {
   const Button(this.text, this.onPressed, {Key? key}) : super(key: key);
@@ -9,18 +10,20 @@ class Button extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsetsDirectional.all(12),
-      child: RawMaterialButton(
-        fillColor: const Color(0xffEEB868),
-        onPressed: () {
-          onPressed();
-        },
-        child: Text(text, textAlign: TextAlign.center, style: const TextStyle(color: Colors.black, fontSize: 20)),
-        shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(500)),
-        elevation: 1,
-        constraints: const BoxConstraints(minWidth: 200.0, minHeight: 50),
-        padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
-      )
-    );
+        margin: const EdgeInsetsDirectional.all(12),
+        child: RawMaterialButton(
+          fillColor: totorYellow,
+          onPressed: () {
+            onPressed();
+          },
+          child: Text(text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.black, fontSize: 20)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(500)),
+          elevation: 1,
+          constraints: const BoxConstraints(minWidth: 200.0, minHeight: 50),
+          padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+        ));
   }
 }
